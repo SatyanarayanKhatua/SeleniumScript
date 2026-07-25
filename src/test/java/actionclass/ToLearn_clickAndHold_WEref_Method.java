bae6c25d5f4efcc3c1910890fdb68647e4b8d6f7
Satya <satyaknk2000@gmail.com>
2026-07-25 21:52:08 +0530

    clickAndHold method by passing WebElement reference

---
+++ src/test/java/actionclass/ToLearn_clickAndHold_WEref_Method.java
@@ -0,0 +1,43 @@
+package actionclass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ToLearn_clickAndHold_WEref_Method {
+
+	@Test
+	public void test() throws InterruptedException {
+		
+		//Create the object of ChromeDriver class
+		WebDriver driver = new ChromeDriver();
+		
+		//maximize the browser
+		driver.manage().window().maximize();
+		
+		//implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//navigate to url
+		driver.get("https://excalidraw.com/");
+		
+		//hard wait
+		Thread.sleep(2000);
+		
+		//create object for Actions class
+		Actions actions = new Actions(driver);
+		
+		//Double click on web page
+		actions.clickAndHold().perform();
+		
+		//hard wait
+		Thread.sleep(3000);
+		
+		//Release the holding
+		actions.release().perform();
+		
+	}
+}
