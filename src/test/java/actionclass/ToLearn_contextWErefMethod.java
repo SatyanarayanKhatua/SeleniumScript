9f49bfd2b1753d1105404f227aca9a1cd6207b76
Satya <satyaknk2000@gmail.com>
2026-07-25 21:55:23 +0530

    contextClick method by WebElement reference

---
+++ src/test/java/actionclass/ToLearn_contextWErefMethod.java
@@ -0,0 +1,41 @@
+package actionclass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ToLearn_contextWErefMethod {
+
+	@Test
+	public void test() throws InterruptedException {
+		
+		//Create the object of ChromeDriver class
+		WebDriver driver = new ChromeDriver();
+		
+		//maximize the browser
+		driver.manage().window().maximize();
+		
+		//implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//navigate to url
+		driver.get("https://excalidraw.com/");
+		
+		//hard wait
+		Thread.sleep(2000);
+		
+		//create object for Actions class
+		Actions actions = new Actions(driver);
+		
+		//find the element by xpath
+		WebElement element = driver.findElement(By.xpath("//button[@id='radix-:r0:']"));
+		
+		//Right click on the web page
+		actions.contextClick(element).perform();
+	}
+}
