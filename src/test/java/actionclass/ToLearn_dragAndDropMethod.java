9b71c0f6edbf796c21d0be4032f7371849acb3fc
Satya <satyaknk2000@gmail.com>
2026-07-26 23:27:21 +0530

    Drag and Drop operation

---
+++ src/test/java/actionclass/ToLearn_dragAndDropMethod.java
@@ -0,0 +1,46 @@
+package actionclass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class ToLearn_dragAndDropMethod {
+
+	@Test
+	public void test() throws InterruptedException {
+		
+		//Create the object of ChromeDriver class
+		WebDriver driver = new ChromeDriver();
+		
+		//maximize the browser
+		driver.manage().window().maximize();
+		
+		//implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//navigate to url
+		driver.get("https://demoqa.com/droppable");
+		
+		//hard wait
+		Thread.sleep(2000);
+		
+		//create object for Actions class
+		Actions actions = new Actions(driver);
+		
+		//Web Element source
+		WebElement source = driver.findElement(By.xpath("//div[@id='draggable']"));
+		
+		//Web Element target
+		WebElement traget = driver.findElement(By.xpath("//div[@id='droppable']"));
+		
+		//Drag and drop by Web Element source and Web Element target
+		actions.dragAndDrop(source, traget).perform();
+		
+
+	}
+}
